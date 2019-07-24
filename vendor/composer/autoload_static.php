0cd61e9573232fdd8929b165b03e6b15507fc684
andersonayres <andersongonzaga82@gmail.comgit config --global user.name andersonayrescommit -m first>
2019-07-23 23:53:19 -0300

    Removendo arquivos de config

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit26b9a2211839f91bd22ec4a4a46c5639
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit26b9a2211839f91bd22ec4a4a46c5639
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit26b9a2211839f91bd22ec4a4a46c5639::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit26b9a2211839f91bd22ec4a4a46c5639::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit26b9a2211839f91bd22ec4a4a46c5639::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit26b9a2211839f91bd22ec4a4a46c5639::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit26b9a2211839f91bd22ec4a4a46c5639::$classMap;
 
